869da2067d1550db4f06ffe752b89438e3220750
nickolay <nickolay8@gmail.com>
2021-08-21 09:29:24 +0300

    reproduce

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
 import { SOME } from "./another.js"
 
-import { it } from 'https://cdn.jsdelivr.net/npm/@bryntum/siesta@latest/index.js'
-
 window.CLIENT = () => SOME
 
+import { it } from 'https://cdn.jsdelivr.net/npm/@bryntum/siesta@latest/index.js'
 it('', () => {})
+
+
