ff4d20d3381b181bfd75800c20233989494a2da5
nickolay <nickolay8@gmail.com>
2021-10-29 17:32:10 +0300

    reproduce

--- index.ts
+++ index.ts
@@ -1,4 +1,21 @@
+const api = () : {
+    Sym1 : () => number,
+    Sym2 : () => boolean
+} => {
+    return null
+}
+
 /**
  * Important constant
  */
-export const Yo = 1
+export const {
+    /**
+     * Docs for Sym1
+     */
+    Sym1,
+
+    /**
+     * Docs for Sym2
+     */
+    Sym2
+} = api()
